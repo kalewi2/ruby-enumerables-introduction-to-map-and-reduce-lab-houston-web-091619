5579b4f1fcfd9c6128fcf4acae731aa4f8f32aa6
Kim Lewis <kalewi2@gmail.com>
2019-09-04 15:14:42 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,31 @@
 # My Code here....
+def map_to_negativize(source_array)
+  source_array = [1,2,3, -9]
+  source_array.map { |i| i*-1 }
+end
+
+def map_to_no_change(dune)
+  source_array = ["paul", "gurney", "vladimir", "jessica", "chani"]
+  source_array.map{|i| i}
+end
+
+def map_to_double(source_array)
+  source_array = [1,  2, 3, -9]
+  source_array.map { |i| i * 2}
+end
+
+def map_to_square(source_array)
+  source_array = [1, 2, 3, -9]
+  source_array.map{|i| i ** 2}
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  source_array.reduce(starting_point, :+) 
+end
+
+def reduce_to_all_true(source_array, starting_point = 0)
+  
+end
+
+def reduce_to_any_true()
+end
